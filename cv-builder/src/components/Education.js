7d256756f28e1ec21f5cfd4fde699bb4f71be886
Ryan Jackson <57965902+Ryanj24@users.noreply.github.com>
2023-05-10 09:24:14 +0100

    Added styling for education component

--- cv-builder/src/components/Education.js
+++ cv-builder/src/components/Education.js
@@ -3,7 +3,37 @@ import '../styling/Education.css'
 
 const Education = () => {
   return (
-    <div></div>
+    <fieldset className='education-container'>
+        <legend>Education</legend>
+        <div className='institution-container'>
+          <div className='institution-input'>
+              <label htmlFor='institution'>Institution</label>
+              <input type= 'text' placeholder='Institution Name' id='institution'></input>
+          </div>
+          <div className='study-title-input'>
+              <label htmlFor='study-title'>Title of Study</label>
+              <input type= 'text' placeholder='Title of Study' id='study-title'></input>
+          </div>
+          <div className='location-input'>
+            <label htmlFor='location'>Location</label>
+              <input type= 'text' placeholder='Location' id='location'></input>
+          </div>
+          <div className='study-duration-input'>
+            <div className='start-date-input'>
+              <label htmlFor='start'>Start Date</label>
+              <input type= 'date' id='start'></input>
+            </div>
+            <div className='end-date-input'>
+              <label htmlFor='end'>End Date</label>
+              <input type= 'date' id='end'></input>
+            </div>
+          </div>
+          <div className='btn-container'>
+              <button type='button' id='add-btn'>Add New</button>
+              <button type='button' id='remove-btn'>Remove</button>
+          </div>
+        </div>
+    </fieldset>
   )
 }
 
--- cv-builder/src/components/Experience.js
+++ cv-builder/src/components/Experience.js
@@ -5,28 +5,46 @@ const Experience = () => {
   return (
     <fieldset className='experience-container'>
         <legend>Professional Experience</legend>
-        <div className='company-input'>
-            <label htmlFor='company'>Company Name</label>
-            <input type= 'text' placeholder='Company Name' id='company'></input>
-        </div>
-        <div className='position-input'>
-            <label htmlFor='position'>Position</label>
-            <input type= 'text' placeholder='Position' id='position'></input>
-        </div>
-        <div className='location-input'>
-          <label htmlFor='location'>Location</label>
-            <input type= 'text' placeholder='Location' id='location'></input>
-        </div>
-        <div className='job-duration-input'>
-          <div className='start-date-input'>
-            <label htmlFor='start'>Start Date</label>
-            <input type= 'date' id='start'></input>
+        <div className='company-container'>
+          <div className='company-input'>
+              <label htmlFor='company'>Company Name</label>
+              <input type= 'text' placeholder='Company Name' id='company'></input>
           </div>
-          <div className='end-date-input'>
-            <label htmlFor='end'>End Date</label>
-            <input type= 'date' id='end'></input>
+          <div className='position-input'>
+              <label htmlFor='position'>Position</label>
+              <input type= 'text' placeholder='Position' id='position'></input>
+          </div>
+          <div className='location-input'>
+            <label htmlFor='location'>Location</label>
+              <input type= 'text' placeholder='Location' id='location'></input>
+          </div>
+          <div className='job-duration-input'>
+            <div className='start-date-input'>
+              <label htmlFor='start'>Start Date</label>
+              <input type= 'date' id='start'></input>
+            </div>
+            <div className='end-date-input'>
+              <label htmlFor='end'>End Date</label>
+              <input type= 'date' id='end'></input>
+            </div>
+          </div>
+          <div className='tasks-input'>
+              <label>Main Tasks of Role</label>
+              <label htmlFor='task1'>Task 1</label>
+              <input type= 'text' id='task1' placeholder='Task Description'></input>
+
+              <label htmlFor='task2'>Task 2</label>
+              <input type= 'text' id='task2' placeholder='Task Description'></input>
+
+              <label htmlFor='task3'>Task 3</label>
+              <input type= 'text' id='task3' placeholder='Task Description'></input>
+          </div>
+          <div className='btn-container'>
+              <button type='button' id='add-btn'>Add New</button>
+              <button type='button' id='remove-btn'>Remove</button>
           </div>
         </div>
+
     </fieldset>
   )
 }
